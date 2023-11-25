fbadd769721ada295e1386de5a5a2aa492d62298
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2023-11-25 15:18:09 +0000

    Ivanov_DI_01_11

--- Ivanov_DI_01_11.py
+++ Ivanov_DI_01_11.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import numpy as np
 
+def proportion_of_education():
+    
+    df = pd.read_csv("assets/NISPUF17.csv", index_col = 0)
+    
+    EDUS = df['EDUC1']
+    edus = np.sort(EDUS.values)
+    
+    poe = {"less than high school": 0,
+        "high school": 0,
+        "more than high school but not college": 0,
+        "college": 0}
+    n = len(edus)
+    
+    poe["less than high school"] = np.sum(edus == 1)/n
+    poe["high school"] = np.sum(edus == 2)/n
+    poe["more than high school but not college"] = np.sum(edus == 3)/n
+    poe["college"] = np.sum(edus == 4)/n
+    
+    return poe
+    raise NotImplementedError()
