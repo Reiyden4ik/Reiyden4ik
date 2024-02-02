11b31fd840709a75950d8a988ff0baef7e022dab
Reiyden4ik <reiyden4ik123@vk.com>
2024-02-02 20:33:42 +0300

    end

--- Studing/trying files/trying_file.py
+++ Studing/trying files/trying_file.py
@@ -1,4 +1,10 @@
-a = int(input())
-b = int(input())
-print(a+b)
-print("lol")
+class Person:
+  Age = int(input())
+  Sex = str(input())
+  Name = str(input())
+  def set_parameters(self, high, weight):
+    self.high = high
+    self.weight = weight
+pr = Person()
+pr.set_parameters(170, 72)
+print('lol')
