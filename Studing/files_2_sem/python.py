810490800cd311a0292841f0576f3489479269a6
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-02-02 20:35:58 +0300

    Update python.py
    Signed-off-by: Даниил <144435096+Reiyden4ik@users.noreply.github.com>

---
+++ Studing/files_2_sem/python.py
@@ -0,0 +1,10 @@
+class Person:
+  Age = int(input())
+  Sex = str(input())
+  Name = str(input())
+  def set_parameters(self, high, weight):
+    self.high = high
+    self.weight = weight
+pr = Person()
+pr.set_parameters(170, 72)
+print('lol')
