c3f020f7f8adc99b2ca5c36c83a0df66d6e4da50
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-04-05 12:21:57 +0000

    done

--- Studing/files_2_sem/Ivanov_DI_01_7.py
+++ Studing/files_2_sem/Ivanov_DI_01_7.py
@@ -8,12 +8,34 @@ class Ratiofraction:
         if den > 0:
             return True
         raise ValueError("den must be natural!")
+    def verify_data(cls, other):
+        if not isinstance(other, (int, Ratiofraction)):
+            raise TypeError("Операнд справа должен быть целым числом или объектом эксземпляра класса Clock")
+        return other if isinstance(other, int) else other.rum
+        return other if isinstance(other, int) else other.den
+
     def __eq__(self, other):
-        return self.rum == other.rum and self.den == other.den
-    def __hash__(self):
-        return hash((self.rum, self.den))
-rt1 = Ratiofraction(int(input('Числитель 1 дроби')), int(input('Знаменатель 1 дроби')))
-rt2 = Ratiofraction(int(input('Числитель 2 дроби')), int(input('Знаменатель 2 дроби')))
+        sc = self.verify_data(other)
+        return self.rum == sc
+        return self.den == sc
+
+    def __lt__(self, other):
+        sc = self.verify_data(other)
+        return self.rum < sc
+        return self.den == sc
+
+    def __ge__(self, other):
+        sc = self.verify_data(other)
+        return self.rum >= sc
+        return self.den == sc
+
+rt1 = Ratiofraction(int(input('Числитель 1 дроби ')), int(input('Знаменатель 1 дроби ')))
+rt2 = Ratiofraction(int(input('Числитель 2 дроби ')), int(input('Знаменатель 2 дроби ')))
+if rt1.rum == rt1.den:
+    print('first ratiofraction is 1')
+    if rt2.rum == rt2.den:
+        print('second ratiofraction is 1')
 if rt1.rum == rt1.den and rt2.rum == rt2.den:
-    print('1 = 1', 'they both equal')
+    print('they both equal and ')
+
 
