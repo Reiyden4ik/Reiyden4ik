2006bf8e43b0e78e68f0204406952d97d4486f44
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-04-12 11:27:05 +0000

    done

--- Studing/files_2_sem/Ivanov_DI_01_8.py
+++ Studing/files_2_sem/Ivanov_DI_01_8.py
@@ -1,5 +1,38 @@
-write me the python code for the following task: “Implement the class structure of things in the room (to choose from): refrigerator, closet, bedside table...Implement the “put” and “take” methods:
-scissors
-book
-pencil
-apple"
+class Thing:
+    def __init__(self, name):
+        self.name = name
+        self.content = []
+
+    def put(self, item):
+        self.content.append(item)
+
+    def take(self, item):
+        if item in self.content:
+            self.content.remove(item)
+        else:
+            print(f"{item} not found in {self.name}")
+
+class Refrigerator(Thing):
+    pass
+
+class Closet(Thing):
+    pass
+
+class BedsideTable(Thing):
+    pass
+
+# Create instances of the classes
+refrigerator = Refrigerator("refrigerator")
+closet = Closet("closet")
+bedside_table = BedsideTable("bedside table")
+
+# Put items into the things
+refrigerator.put("apple")
+closet.put("scissors")
+closet.put("book")
+bedside_table.put("pencil")
+
+# Take items from the things
+bedside_table.take("pencil")
+closet.take("scissors")
+refrigerator.take("apple")
