200a0901b5c024bfb456ab1ce5a27116311c153f
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-04-12 14:24:39 +0300

    Update Ivanov_DI_01_8.py

---
+++ Studing/files_2_sem/Ivanov_DI_01_8.py
@@ -0,0 +1,5 @@
+write me the python code for the following task: “Implement the class structure of things in the room (to choose from): refrigerator, closet, bedside table...Implement the “put” and “take” methods:
+scissors
+book
+pencil
+apple"
