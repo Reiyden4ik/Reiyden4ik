39762c51dc5e86f9eae0f26b939fc3303fe87435
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-04-12 12:31:16 +0000

    done

--- Studing/files_2_sem/Ivanov_DI_01_8.py
+++ Studing/files_2_sem/Ivanov_DI_01_8.py
@@ -1,38 +1,71 @@
 class Thing:
     def __init__(self, name):
         self.name = name
-        self.content = []
+        self.contents = []
 
     def put(self, item):
-        self.content.append(item)
+        self.contents.append(item.name)
 
-    def take(self, item):
-        if item in self.content:
-            self.content.remove(item)
+    def take(self, item_name):
+        if item_name in self.contents:
+            self.contents.remove(item_name)
         else:
-            print(f"{item} not found in {self.name}")
+            print(f"{item_name} not found in {self.name}")
 
 class Refrigerator(Thing):
-    pass
+    def __init__(self):
+        super().__init__("Refrigerator")
 
-class Closet(Thing):
-    pass
+    def put(self, item):
+        if item.is_edible:
+            super().put(item)
+        else:
+            print(f"{item.name} is not edible, cannot put in refrigerator")
 
 class BedsideTable(Thing):
-    pass
+    def __init__(self):
+        super().__init__("Bedside Table")
 
-# Create instances of the classes
-refrigerator = Refrigerator("refrigerator")
-closet = Closet("closet")
-bedside_table = BedsideTable("bedside table")
+    def put(self, item):
+        if item.is_writing_material:
+            super().put(item)
+        else:
+            print(f"{item.name} is not writing material, cannot put in bedside table")
 
-# Put items into the things
-refrigerator.put("apple")
-closet.put("scissors")
-closet.put("book")
-bedside_table.put("pencil")
+class Closet(Thing):
+    def __init__(self):
+        super().__init__("Closet")
 
-# Take items from the things
-bedside_table.take("pencil")
-closet.take("scissors")
-refrigerator.take("apple")
+    def put(self, item):
+        if item.is_clothes:
+            super().put(item)
+        else:
+            print(f"{item.name} is not clothes, cannot put in closet")
+
+class Item:
+    def __init__(self, name, is_edible=False, is_writing_material=False, is_clothes=False):
+        self.name = name
+        self.is_edible = is_edible
+        self.is_writing_material = is_writing_material
+        self.is_clothes = is_clothes
+
+scissors = Item("Scissors", is_writing_material=True)
+book = Item("Book", is_writing_material=True)
+pencil = Item("Pencil", is_writing_material=True)
+apple = Item("Apple", is_edible=True)
+shirt = Item("Shirt", is_clothes=True)
+
+refrigerator = Refrigerator()
+bedside_table = BedsideTable()
+closet = Closet()
+
+refrigerator.put(apple)
+bedside_table.put(scissors)
+bedside_table.put(book)
+bedside_table.put(pencil)
+closet.put(shirt)
+closet.take(shirt)
+
+print("In the refrigerator:", refrigerator.contents)
+print("In the bedside table:", bedside_table.contents)
+print("In the closet:", closet.contents)
