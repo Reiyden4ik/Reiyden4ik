228b604f0815b23219e71c020897e81ce62b7537
daniil ivanov <reiyden4ik123@vk.com>
2024-04-27 00:21:38 +0300

    wow

---
+++ Studing/trying files/site_backend.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'name': 'John Doe',
+        'age': 30,
+        'city': 'New York'
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#http://localhost:5000
+#http://localhost:5000/api/data
