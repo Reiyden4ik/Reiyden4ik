124a9f0c4fcc99cdd627c8e558edd81afc601b6c
daniil ivanov <reiyden4ik123@vk.com>
2024-04-29 02:28:56 +0300

    done

--- Studing/tryingfiles/site_backend.py
+++ Studing/tryingfiles/site_backend.py
@@ -1,12 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World!"
+def hello_world():
+    return 'Hello from Flask!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-#http://localhost:5000
