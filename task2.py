21e3a233dbe71ae63b2b66962348319a0888a759
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-10-27 03:34:11 +0300

    Update task2.py
    Немного подредактировал код, теперь даже при округлении вниз ответ остается правильным
    
    Signed-off-by: Даниил <144435096+Reiyden4ik@users.noreply.github.com>

--- task2.py
+++ task2.py
@@ -1,12 +1,13 @@
+from math import floor
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-money_capital = 0
-while months!=0:
-    money_capital = money_capital+spend-salary
-    months-=1
-    spend*=(1+increase)
-# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-months=10
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+
+money_capital = 0   # Подушка безопасности, чтобы протянуть 10 месяцев без долгов
+
+for i in range(months):
+    money_capital = money_capital + spend - salary
+    spend = spend*(1 + increase)
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", floor(money_capital))
