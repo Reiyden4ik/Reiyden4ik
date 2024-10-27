0fb039a831d8fbd63dd1535f8515b937d42dd0b0
Даниил <144435096+Reiyden4ik@users.noreply.github.com>
2024-10-27 22:43:50 +0300

    Update task2.py
    Переделал еще раз
    
    Signed-off-by: Даниил <144435096+Reiyden4ik@users.noreply.github.com>

--- task2.py
+++ task2.py
@@ -1,4 +1,3 @@
-from math import floor
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -6,8 +5,8 @@ increase = 0.03  # Ежемесячный рост цен
 
 money_capital = 0   # Подушка безопасности, чтобы протянуть 10 месяцев без долгов
 
-for i in range(months):
-    money_capital = money_capital + spend - salary
-    spend = spend*(1 + increase)
+for _ in range(months):
+    money_capital += spend - salary
+    spend *= 1 + increase
 
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", floor(money_capital))
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
