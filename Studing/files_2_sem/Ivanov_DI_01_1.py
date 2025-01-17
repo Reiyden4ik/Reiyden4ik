662c6af57bbca32f227d27657617a191b8eae1c8
daniil ivanov <reiyden4ik123@vk.com>
2025-01-17 05:28:33 +0300

    done

--- Studing/files_2_sem/Ivanov_DI_01_1.py
+++
@@ -1,9 +0,0 @@
-class Person:
-  Age = int(input())
-  Sex = str(input())
-  Name = str(input())
-  def set_parameters(self, high, weight):
-    self.high = high
-    self.weight = weight
-pr = Person()
-pr.set_parameters(170, 72)
